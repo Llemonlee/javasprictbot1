ba37de074ced6e458c6161851814abb612714f80
Llemonlee <30706lilee@gmail.com>
2024-07-13 21:42:40 +0800

    1.0

---
+++ src/bot.js
@@ -0,0 +1,15 @@
+import{ Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.token);
